5fae2813adf88601bc40f6a51051eee9c3930e9b
datata <dani@danitarazona.dev>
2023-11-28 12:11:08 +0100

    feat: healthcheck endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get(
-    '/user',
-    function (Request $request) {
-        return $request->user();
-    }
-);
+// Route::middleware('auth:sanctum')->get(
+//     '/user',
+//     function (Request $request) {
+//         return $request->user();
+//     }
+// );
+
+Route::get('/api', function(Request $request) {
+    return response()->json(
+        [
+            "success" => true,
+            "message" => "Healthcheck ok"
+        ],
+        Response::HTTP_OK //code status 200
+    );
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('bienvenido');
-});
+// Route::get('/', function () {
+
+//     // recuperar usuarios
+
+//     $users = [];
+//     return view('bienvenido', [$users]);
+// });
