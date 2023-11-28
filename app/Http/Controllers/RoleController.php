c1d0fa1436dc9fee29e7301281f02ad3b71d8c6b
datata <dani@danitarazona.dev>
2023-11-28 12:57:06 +0100

    feat: create role with query builder

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,10 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
 
 class RoleController extends Controller
 {
     public function createRole(Request $request) {
+        try {
+            $name = $request->input('name');
+
+            // query builder
+            DB::table('roles')->insert(
+                [
+                    'name' => $name,
+                    // 'created_at' ...
+                ]
+            );
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Role created"
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error creating role"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+
         return 'Create Role';
     }
 
