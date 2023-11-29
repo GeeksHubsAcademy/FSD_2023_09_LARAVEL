0d072c2201d5dc89bcbdc76e846587b92aca1f3e
datata <dani@danitarazona.dev>
2023-11-29 12:26:48 +0100

    update: register attributes validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,28 +5,41 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
         try {
-            // recuperar info del body
-            $name = $request->input('name');
-            $password = $request->input('password');
-            $email = $request->input('email');
+            // validar la data del user
+            $validator = $this->validateDataUser($request);
+     
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "Error registering user",
+                        "error" => $validator->errors()
+                    ],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
 
-            // validar esa info
+            // // recuperar info del body
+            // $name = $request->input('name');
+            // $password = $request->input('password');
+            // $email = $request->input('email');
 
-            // tratar los datos
-            $encryptedPassword = bcrypt($password);
+            // // tratar los datos
+            // $encryptedPassword = bcrypt($password);
 
             // guardar esos datos
             $newUser = User::create(
                 [
-                    "name" => $name,
-                    "email" => $email,
-                    "password" => $encryptedPassword
+                    "name" => $request->input('name'),
+                    "email" => $request->input('email'),
+                    "password" => bcrypt($request->input('password'))
                 ]
             );
 
@@ -53,5 +66,15 @@ class AuthController extends Controller
                 Response::HTTP_INTERNAL_SERVER_ERROR
             );
         }
+    }
+
+    private function validateDataUser(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:6|max:12',
+        ]);
+
+        return $validator;
     }
 }
